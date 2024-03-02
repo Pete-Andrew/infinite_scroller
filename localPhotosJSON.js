3dadb910175b21c26aee1ef7b6980eaba4660937
Peter Andrew <peter.a.andrew@gmail.com>
2024-03-02 13:58:04 +0000

    added console.log for check box 1

--- localPhotosJSON.js
+++ localPhotosJSON.js
@@ -10,16 +10,23 @@ const localPhotosArray = [
     "https://i.pinimg.com/564x/a0/a6/e0/a0a6e06a05683797183d317ab7cb3c94.jpg",
     "https://i.pinimg.com/564x/35/19/e6/3519e633d3491f26c33fd13709a5d3ce.jpg",
     "https://i.pinimg.com/564x/23/da/65/23da65c3989c55d85d500c6ca4d19bdc.jpg",
+    "https://i.pinimg.com/564x/67/56/c8/6756c8d24ff2270dcdf10f368bb62337.jpg",
     "https://i.pinimg.com/564x/8b/b9/91/8bb99160fe2050961a31c0d7a4e24dc3.jpg",
     "https://i.pinimg.com/564x/10/ac/d8/10acd8cc187612178328c66f32cbea07.jpg",
     "https://i.pinimg.com/564x/66/24/f4/6624f4a2ed9b3b49688ecb22fd3ebc8f.jpg",
     "https://i.pinimg.com/564x/40/5b/af/405baf715b6cc197d5b353f20b952bc1.jpg",
     "https://i.pinimg.com/564x/4e/24/20/4e2420041b5b10029a278143f4625a0f.jpg",
+    "https://i.pinimg.com/564x/dc/fd/4a/dcfd4a172d248f8320ffe90efd5447a0.jpg",
     "https://i.pinimg.com/564x/6b/31/80/6b31802e31445d4cdcc0a0dd40cf8642.jpg",
     "https://i.pinimg.com/564x/ac/a3/ff/aca3ff45185b700c419b1f613c41e2b9.jpg",
     "https://i.pinimg.com/564x/c1/16/d2/c116d253503c99a91b6cb580685a42ca.jpg",
     "https://i.pinimg.com/564x/b7/8c/c8/b78cc8721d265e07059ab8c55883207b.jpg",
     "https://i.pinimg.com/736x/e5/a2/71/e5a271a3e214bec34ac9d9cb822434c6.jpg",
     "https://i.pinimg.com/564x/d7/a8/a4/d7a8a4b677e5f3b7f959d7931a90f58f.jpg",
+    "https://i.pinimg.com/564x/9b/4b/7a/9b4b7a045e2afcefeb229b77a803fae2.jpg",
+    "https://i.pinimg.com/564x/ac/23/d6/ac23d61e38a720303360a4ba260fdf31.jpg",
+    "https://i.pinimg.com/564x/55/f7/9e/55f79eca7b520c7f75eb3a7d0861ab11.jpg",
+
+
     
 ];
